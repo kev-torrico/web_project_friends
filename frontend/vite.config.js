bee92b09f66d8db78f2b296bf1274b0d87132b64
KevinVillca <kevtorricov@gmail.com>
2025-04-04 01:36:31 -0400

    Increase chunks size limit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: { port: 3000 },
+  build: {
+    chunkSizeWarningLimit: 1000, // size in KB
+  },
 });
